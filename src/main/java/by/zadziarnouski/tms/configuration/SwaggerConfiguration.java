2f705586153075d9e9cc58cab29b3abd4cf4be37
taras <taras.zadziarnouski@gmail.com>
2021-07-20 13:11:46 +0300

    connected PostgreSQL, created 2 profiles and each has its own properties file and refactored pom.xml

--- src/main/java/by/zadziarnouski/tms/configuration/SwaggerConfiguration.java
+++ src/main/java/by/zadziarnouski/tms/configuration/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package by.zadziarnouski.tms.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +11,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfiguration {
+
     @Bean
+    @Profile("dev")
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
